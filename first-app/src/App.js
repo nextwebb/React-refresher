d6d888bfda48d317777c8848e236f60af8ba6f21
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-06 18:36:57 +0100

    styles

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,21 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './components/header'
 import Footer from './components/footer'
+import styled from 'styled-components'
 
 
 function  createAlert(){
   alert('hello, you clicked me!')
 }
 
+const pStyle = {
+  fontSize:'2em',
+  color: 'red',
+}
+const Paragraph = styled.p`
+  font-size: 3em
+  color: green;
+`;
 
 function App() {
-
     return (
       <div className="App">
         <Header info="This is my message" myNumber="6" />
-          <p>Main content</p>
+        <p style={pStyle}>Main content</p>
+        <Paragraph>New styled</Paragraph>
         <Footer trademark="page by nextwebb" myalert={createAlert} />
-          
       </div>
     );
  
