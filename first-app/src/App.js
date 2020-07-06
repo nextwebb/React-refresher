564de250c26cffe4cd4f19f7f7a4402284ea01a1
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-06 04:05:47 +0100

    events

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Header from './components/header'
 import Footer from './components/footer'
 
+function  createAlert(){
+  alert('hello, you clicked me!')
+}
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +14,7 @@ function App() {
       <Header info="This is my message" myNumber="6" />
       <Header info="This is my message" myNumber="4" />
         <p>Main content</p>
-      <Footer trademark="page by nextwebb" />
+      <Footer trademark="page by nextwebb" myalert={createAlert} />
     </div>
   );
 }
--- first-app/src/components/footer.js
+++ first-app/src/components/footer.js
@@ -1,8 +1,21 @@
 import React, { Component} from 'react';
 
 class Footer extends Component{
+    changed(e) {
+        console.log('changed')
+    }
+   
     render() {
-        return <h2>{this.props.trademark} </h2>
+        return (
+            <div>
+                <h2 onClick={this.props.myalert}>
+                    {this.props.trademark} 
+                </h2>
+                <input  onChange={this.changed} />
+            </div>
+        )
+          
+        
     }
 }
 
