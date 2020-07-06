d10af056e1e78c325073300646be8f05d138f690
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-06 17:23:55 +0100

    routes

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header'
 import Footer from './components/footer'
 
+
 function  createAlert(){
   alert('hello, you clicked me!')
 }
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './components/header'
+import Footer from './components/footer'
 import * as serviceWorker from './serviceWorker';
+import { Route,BrowserRouter} from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={App}></Route>
+      <Route path="/header" component={Header}></Route>
+      <Route path="/footer" component={Footer}></Route>
+    </div>
+  </BrowserRouter>
+)
+ReactDOM.render(routing, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
