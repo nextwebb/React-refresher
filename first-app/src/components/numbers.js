e7b0a40d924412036363e32f7943ccf08d74d3b0
nextwebb <oaikhenahpeterson@gmail.com>
2020-07-06 23:40:27 +0100

    react hooks-effect

--- first-app/src/components/numbers.js
+++ first-app/src/components/numbers.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Numbers = () => {
     //destructure of an array
@@ -7,6 +7,13 @@ const Numbers = () => {
     const addNumber = () => {
         setNumbers([...numbers,'four'])
     }
+    // listens for states changes and component mount
+    useEffect(() => {
+        console.log('our use effect trigers only on numbers')
+    }, [numbers])
+    const addLetter = () => {
+        setLetters([...letters,'f'])
+    }
         return (
             <div>
                 <h1>Numbers</h1>
@@ -14,6 +21,10 @@ const Numbers = () => {
                     return <h4>{num}</h4>
                 })}
                 <button onClick={addNumber}>Add a number</button>
+                {letters.map( letter=> {
+                    return <h4>{letter}</h4>
+                })}
+                <button onClick={addLetter}>Add a letter</button>
             </div>
         )
 }
